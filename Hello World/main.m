3d899826cf3baa56043e7f0273e58a613d0c6aca
Masahrio <masahiro_okubo@rakudo.io>
2017-12-15 16:49:59 +0900

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by 大久保将広 on 2017/12/15.
+//  Copyright © 2017年 大久保将広. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
